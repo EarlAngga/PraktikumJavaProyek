520df840b4b0074fc2931fa073e1c06b87ccf960
EarlAngga <erlangga1898@gmail.com>
2022-07-01 18:04:00 +0800

    Membuat KabupatenViewFrame

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
 import helpers.Koneksi;
+import frame.KabupatenViewFrame;
 
 public class Main {
     public static void main(String[] args) {
-        Koneksi.getConnection();
+//        Koneksi.getConnection();
+        KabupatenViewFrame viewFrame = new KabupatenViewFrame();
+        viewFrame.setVisible(true);
     }
 }
